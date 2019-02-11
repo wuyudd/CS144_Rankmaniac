823c656d505ee79303420ed57583cb3b928afcef
WilliamGu <williamgu@WilliamGudeMacBook-Air.local>
2019-02-11 17:38:02 -0500

    pagerank_reduce updates

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
 import sys
+import collections
 
 #
 # This program simply represents the identity function.
 #
 
+def reducer(key,vals):
+	alpha = 0.85
+	sum = 1-alpha
+	fir = key
+	sec = ''
+	for v in vals:
+		if v[0] == '+':
+			sum += flaot(v[1:])
+		else:
+			sec = v
+	sec = sec.split(',')
+	sec[1] = sec[0]
+	sec[0] = str(sum)
+	s = fir+'\t'+sec.join(',')
+	sys.stdout.write(s)
+
+data = collections.defaultdict(list)
 for line in sys.stdin:
-    sys.stdout.write(line)
+    key,val = readline(line)
+    s = line.split('\t')
+    data[key].append(val)
+
+for key,val in data.items():
+	reducer(key,val)
+
 
